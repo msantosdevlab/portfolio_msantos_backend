e0bc4994faf121b5152543cc00741afb6859365c
msantos-lab <msantosdev.lab@gmail.com>
2025-02-08 15:32:29 +0100

    Register 'Introduction' model in 'admin.py' for content management

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Introduction
 
 # Register your models here.
+@admin.register(Introduction)
+class IntroductionAdmin(admin.ModelAdmin):
+    list_display = ("eyebrow", "title", "description",)
